e9fa14390ce0d0e7bcf983c550aa29f270340180
m.mollaramezani <ramezani.mreza@gmail.com>
2023-07-31 20:21:04 +0330

    add validation messge

--- Hoshmand.Presentation/Validations/AuthenticationValidation.cs
+++ Hoshmand.Presentation/Validations/AuthenticationValidation.cs
@@ -7,18 +7,18 @@ namespace Hoshmand.Presentation.Validations
     {
         public AuthenticationValidation()
         {
-            RuleFor(q => q.Mobile).Must(x => x.StartsWith("09"));
-            RuleFor(q => q.Mobile).Length(11); 
-            RuleFor(q => q.Mobile).NotNull().NotEmpty();
+            RuleFor(q => q.Mobile).Must(x => x.StartsWith("09")).WithMessage("شماره موبایل با 09 شروع شود"); ;
+            RuleFor(q => q.Mobile).Length(11).WithMessage("طول شماره موبایل اشتباه است"); ; 
+            RuleFor(q => q.Mobile).NotNull().NotEmpty().WithMessage("شماره موبایل خالی است"); ;
          
-            RuleFor(q => q.NationalCode).NotNull().NotEmpty();
-            RuleFor(q => q.NationalCode).Length(11);
+            RuleFor(q => q.NationalCode).NotNull().NotEmpty().WithMessage("کد ملی خالی است"); ;
+            RuleFor(q => q.NationalCode).Length(10).WithMessage("طول کد ملی اشتباه است"); ;
 
 
-            RuleFor(q => q.IdCardFrontImage).NotEmpty().NotNull();
-            RuleFor(q => q.IdCardBehindImage).NotEmpty().NotNull();
-            RuleFor(q => q.liveVideo).NotEmpty().NotNull();
-            RuleFor(q => q.SelfiImage).NotEmpty().NotNull();
+            RuleFor(q => q.IdCardFrontImage).NotEmpty().NotNull().WithMessage("تصویر روی کارت ملی خالی است"); ;
+            RuleFor(q => q.IdCardBehindImage).NotEmpty().NotNull().WithMessage("تصویر پشت کارت ملی خالی  است"); ;
+            RuleFor(q => q.liveVideo).NotEmpty().NotNull().WithMessage("ویدیوی خالی است"); ;
+            RuleFor(q => q.SelfiImage).NotEmpty().NotNull().WithMessage("تصویر سلفی خالی است"); ;
         }
     }
 }
